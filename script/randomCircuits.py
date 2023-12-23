7dae5c8c52e09f4ecd72fef3aa9e6024eb1e00c4
Shiwen An <sweynan@icloud.com>
2023-12-23 13:26:39 +0900

    generate random circuit for categorization

---
+++ script/randomCircuits.py
@@ -0,0 +1,6 @@
+################################################    
+# Author: SHiwen An.                           #
+# Date: 2023-12-23.                            #
+# Purpose: Create Simple Gates and Circuit     #
+#          to practice the algorithms          #
+################################################
